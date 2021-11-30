bf110180246ad9206fc803e9a2119d1b925a4ac4
Kevin Decker <KevinDecker64@gmail.com>
2021-11-30 14:25:48 -0600

    displayWord functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,39 @@ import Word from "./components/Word";
 
 import "./App.css";
 
+const words = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "moist",
+  "laceration",
+  "bass",
+  "chastity",
+  "lavender",
+  "blanket",
+  "teeth",
+  "magnets",
+  "weights",
+  "paper",
+  "hair",
+  "curly",
+  "warm",
+  "paperclip",
+  "briefcase",
+  "smells",
+  "soft",
+  "extroverted",
+  "friendly",
+  "chill",
+];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 export default function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
@@ -17,7 +50,7 @@ export default function App() {
       <div className="game-container">
         <Figure />
         <WrongLetters />
-        <Word />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
       </div>
     </>
   );
--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Word({ selectedWord, correctLetters }) {
-  return <div className="word" id="word"></div>;
+  return (
+    <div className="word" id="word">
+      {selectedWord.split("").map((letter, i) => {
+        return (
+          <span className="letter" key={i}>
+            {correctLetters.includes(letter) ? letter : ""}
+          </span>
+        );
+      })}
+    </div>
+  );
 }
